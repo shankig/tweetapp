a6786401a45c165b43ae1f1000ca7a6666a86a6e
Shanki <ssg.bnv@gmail.com>
2015-05-31 19:24:35 +0530

    updated wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, "/home/proj/tweetapp")
 
 from tweetapp import app as application
